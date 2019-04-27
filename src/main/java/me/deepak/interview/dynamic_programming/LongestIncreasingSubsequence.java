734133ce3265783cc47265e593f25a26edf1292b
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-04-27 22:22:23 +0530

    LIS done

--- src/main/java/me/deepak/interview/dynamic_programming/LongestIncreasingSubsequence.java
+++ src/main/java/me/deepak/interview/dynamic_programming/LongestIncreasingSubsequence.java
@@ -4,45 +4,21 @@ import java.util.Arrays;
 
 public class LongestIncreasingSubsequence {
 
-	public static void main(String[] args) {
-		int[] arr = { 10, 22, 9, 33, 21, 50, 41, 60 };
-		int[] lis = lis(arr);
-		for (int i : lis) {
-			System.out.println(i);
-		}
-		// System.out.println("Length of lis is " + lisLength(arr));
+	private LongestIncreasingSubsequence() {
 	}
 
-	private static int lisLength(int[] arr) {
-		int length = arr.length;
-		int[] lis = new int[length];
-
-		/* Initialize LIS values for all indexes */
-		Arrays.fill(lis, 1);
-
-		/* Compute optimized LIS values in bottom up manner */
-		// if arr[i] > arr[j], then lis[i] = max(lis[i], lis[j] + 1)
-		for (int i = 1; i < length; i++) {
-			for (int j = 0; j < i; j++) {
-				if (arr[i] > arr[j] && lis[j] + 1 > lis[i]) {
-					lis[i] = lis[j] + 1;
-				}
-			}
-		}
-
-		/* Pick maximum of all LIS values */
-		return Arrays.stream(lis).max().getAsInt();
-	}
-
-	private static int[] lis(int[] arr) {
+	public static int lisLength(int[] arr) {
 		int length = arr.length;
 		int[] lisLengths = new int[length];
 
-		/* Initialize LIS values for all indexes */
+		// Initialize LIS values for all indexes
 		Arrays.fill(lisLengths, 1);
 
-		/* Compute optimized LIS values in bottom up manner */
-		// if arr[i] > arr[j], then lis[i] = max(lis[i], lis[j] + 1)
+		/*
+		 * Compute optimized LIS values in bottom up manner. if arr[i] > arr[j], then
+		 * lis[i] = max(lis[i], lis[j] + 1)
+		 * 
+		 */
 		for (int i = 1; i < length; i++) {
 			for (int j = 0; j < i; j++) {
 				if (arr[i] > arr[j]) {
@@ -51,17 +27,51 @@ public class LongestIncreasingSubsequence {
 			}
 		}
 
-		/* Pick maximum of all LIS values */
+		// Pick maximum of all LIS values
+		return Arrays.stream(lisLengths).max().getAsInt();
+	}
+
+	public static int[] lis(int[] arr) {
+		int length = arr.length;
+		int[] lisLengths = new int[length];
+
+		// Initialize LIS values for all indexes
+		Arrays.fill(lisLengths, 1);
+
+		/*
+		 * Compute optimized LIS values in bottom up manner. if arr[i] > arr[j], then
+		 * lis[i] = max(lis[i], lis[j] + 1)
+		 * 
+		 */
+		for (int i = 1; i < length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[i] > arr[j]) {
+					lisLengths[i] = Math.max(lisLengths[i], lisLengths[j] + 1);
+				}
+			}
+		}
+
+		// Pick maximum of all LIS values
 		int lisLength = Arrays.stream(lisLengths).max().getAsInt();
 
 		int[] lis = new int[lisLength];
+		// loop on lisLengths
 		for (int i = length - 1; i >= 0; i--) {
+			// if current lisLength == lisLengths[i], add it to lis
 			if (lisLengths[i] == lisLength) {
-				lis[lisLength - 1] = arr[i];
-				lisLength--;
+				lis[--lisLength] = arr[i];
 			}
 		}
 		return lis;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 10, 22, 9, 33, 21, 50, 41, 60 };
+		int[] lis = lis(arr);
+		System.out.println("LIS length is = " + lisLength(arr));
+		for (int i : lis) {
+			System.out.print(i + " ");
+		}
 	}
 
 }
