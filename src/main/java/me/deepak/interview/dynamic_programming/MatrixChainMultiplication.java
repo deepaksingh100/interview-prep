34c3935b2c572b6bf1c5f5d7bbe0431e9dfec0e6
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-04-28 16:23:53 +0530

    Matrix Chain Multiplication done.

--- src/main/java/me/deepak/interview/dynamic_programming/MatrixChainMultiplication.java
+++ src/main/java/me/deepak/interview/dynamic_programming/MatrixChainMultiplication.java
@@ -1,15 +1,38 @@
 package me.deepak.interview.dynamic_programming;
 
+/*
+ * https://www.geeksforgeeks.org/matrix-chain-multiplication-dp-8/
+ * https://www.geeksforgeeks.org/printing-brackets-matrix-chain-multiplication-problem/FO
+*/
+
 public class MatrixChainMultiplication {
 
+	private MatrixChainMultiplication() {
+	}
+
 	public static int matrixChainMultiplication(int[] dimensions) {
-		int n = dimensions.length - 1; // number of matrices
-		int[][] table = new int[n + 1][n + 1];
-		for (int l = 2; l <= n; l++) { // l is chain length
-			for (int i = 1; i <= n - l + 1; i++) {
+		int length = dimensions.length - 1; // number of matrices
+
+		/*
+		 * For simplicity of the program, one extra row and one extra column are
+		 * allocated in table[][]. 0th row and 0th column of table[][] are not used
+		 */
+		int[][] table = new int[length + 1][length + 1];
+		for (int l = 2; l <= length; l++) { // l is chain length
+			for (int i = 1; i <= length - l + 1; i++) {
+
+				// end index of current sequence is j. start index is i. length is l.
 				int j = i + l - 1;
 				table[i][j] = Integer.MAX_VALUE;
+
+				// finding break point k for all i < j
 				for (int k = i; k < j; k++) {
+
+					/*
+					 * table[i][j] = for all i <= k < j min(table[i][k] + table[k + 1][j] +
+					 * dimensions[i - 1] * dimensions[k] * dimensions[j])
+					 * 
+					 */
 					int minCost = table[i][k] + table[k + 1][j] + dimensions[i - 1] * dimensions[k] * dimensions[j];
 					if (minCost < table[i][j]) {
 						table[i][j] = minCost;
@@ -17,28 +40,56 @@ public class MatrixChainMultiplication {
 				}
 			}
 		}
-		return table[1][n];
+		return table[1][length];
 	}
 
 	// exactly implemented according to CLRS book
 	public static void matrixChainMultiplicationSolution(int[] dimensions) {
-		int n = dimensions.length - 1; // number of matrices
-		int[][] table = new int[n + 1][n + 1];
-		int[][] solution = new int[n][n + 1];
-		for (int l = 2; l <= n; l++) { // l is chain length
-			for (int i = 1; i <= n - l + 1; i++) {
+		int length = dimensions.length - 1; // number of matrices
+
+		/*
+		 * For simplicity of the program, one extra row and one extra column are
+		 * allocated in table[][]. 0th row and 0th column of table[][] are not used
+		 */
+		int[][] table = new int[length + 1][length + 1];
+
+		// solution[i][j] stores optimal break point in subexpression from i to j.
+		int[][] solution = new int[length + 1][length + 1];
+
+		/*
+		 * table[i,j] = Minimum number of scalar multiplications needed to compute the
+		 * matrix A[i]A[i+1]...A[j] = A[i..j] where dimension of A[i] is p[i-1] x p[i]
+		 */
+
+		for (int l = 2; l <= length; l++) { // l is chain length
+			for (int i = 1; i <= length - l + 1; i++) {
+
+				// end index of current sequence is j. start index is i. length is l.
 				int j = i + l - 1;
 				table[i][j] = Integer.MAX_VALUE;
+
+				// finding break point k for all i < j
 				for (int k = i; k < j; k++) {
+
+					/*
+					 * table[i][j] = for all i <= k < j min(table[i][k] + table[k + 1][j] +
+					 * dimensions[i - 1] * dimensions[k] * dimensions[j])
+					 * 
+					 */
 					int minCost = table[i][k] + table[k + 1][j] + dimensions[i - 1] * dimensions[k] * dimensions[j];
 					if (minCost < table[i][j]) {
 						table[i][j] = minCost;
+
+						/*
+						 * Each entry solution[i, j] = k shows where to split the product arr i,i+1....j
+						 * for the minimum cost.
+						 */
 						solution[i][j] = k;
 					}
 				}
 			}
 		}
-		print(solution, 1, n);
+		print(solution, 1, length);
 	}
 
 	private static void print(int[][] solution, int i, int j) {
@@ -46,8 +97,9 @@ public class MatrixChainMultiplication {
 			System.out.print("M" + i);
 		} else {
 			System.out.print("(");
-			print(solution, i, solution[i][j]);
-			print(solution, solution[i][j] + 1, j);
+			int k = solution[i][j];
+			print(solution, i, k);
+			print(solution, k + 1, j);
 			System.out.print(")");
 		}
 	}
