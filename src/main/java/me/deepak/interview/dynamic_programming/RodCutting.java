0fdb4fbf9cb75a0bb0094a39827d5edbb911886e
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-04-28 18:31:17 +0530

    Rod Cutting done

--- src/main/java/me/deepak/interview/dynamic_programming/RodCutting.java
+++ src/main/java/me/deepak/interview/dynamic_programming/RodCutting.java
@@ -3,13 +3,27 @@ package me.deepak.interview.dynamic_programming;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * https://www.geeksforgeeks.org/cutting-a-rod-dp-13/
+*/
+
 public class RodCutting {
 
+	private RodCutting() {
+	}
+
 	public static int cutRod(int[] prices, int rodLength) {
+
+		// revenues[i] represents the max revenue for rod length i
 		int[] revenues = new int[rodLength + 1];
+
+		// max revenue from zero length rod is zero, starting from rod length 1
 		for (int i = 1; i <= rodLength; i++) {
 			int maxRevenue = Integer.MIN_VALUE;
+
+			// cutting rod of length i into j and i - j - 1 (0 -> n - 1)
 			for (int j = 0; j < i; j++) {
+				// cutRod(n) = for all i in {0, 1 .. n-1} max(price[i] + cutRod(n-i-1))
 				maxRevenue = Math.max(maxRevenue, prices[j] + revenues[i - j - 1]);
 			}
 			revenues[i] = maxRevenue;
@@ -18,13 +32,24 @@ public class RodCutting {
 	}
 
 	public static List<Integer> cutRodSolution(int[] prices, int rodLength) {
+
+		// revenues[i] represents the max revenue for rod length i
 		int[] revenues = new int[rodLength + 1];
 		int[] solution = new int[rodLength + 1];
+
+		// max revenue from zero length rod is zero, Starting from rod length 1
 		for (int i = 1; i <= rodLength; i++) {
 			int maxRevenue = Integer.MIN_VALUE;
+
+			// cutting rod of length i into j and i - j - 1 (0 -> n - 1)
 			for (int j = 0; j < i; j++) {
+
+				// cutRod(n) = for all i in {0, 1 .. n-1} max(price[i] + cutRod(n-i-1))
 				if (maxRevenue < prices[j] + revenues[i - j - 1]) {
 					maxRevenue = prices[j] + revenues[i - j - 1];
+
+					// hold the optimal size j of the first piece to cut off when solving a
+					// subproblem of size i
 					solution[i] = j;
 				}
 			}
@@ -32,6 +57,8 @@ public class RodCutting {
 		}
 		List<Integer> cutRod = new ArrayList<>();
 		while (rodLength > 0) {
+
+			// adding optimal values to answer
 			cutRod.add(solution[rodLength]);
 			rodLength -= solution[rodLength];
 		}
