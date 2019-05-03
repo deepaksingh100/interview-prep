11ecd980518eb6911993475f32a61f3c748c6f78
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-05-03 15:00:10 +0530

    LRU Cache done

--- src/main/java/me/deepak/interview/lru/LRUCache1.java
+++ src/main/java/me/deepak/interview/lru/LRUCache1.java
@@ -4,70 +4,104 @@ import java.util.HashMap;
 import java.util.Map;
 
 /*
- * LRU Cache implementation using Map & Linked List
+ * LRU Cache implementation using Map & Doubly Linked List
  * 
+ * https://www.geeksforgeeks.org/lru-cache-implementation/
+ * https://www.geeksforgeeks.org/design-a-data-structure-for-lru-cache/
+ * https://leetcode.com/problems/lru-cache/
  */
-
 public class LRUCache1 {
 
 	private int capacity;
-	private Map<Integer, Node> map;
+	private Map<Integer, Node> cache;
 	private Node head;
 	private Node tail;
 
 	public LRUCache1(int capacity) {
 		this.capacity = capacity;
-		this.map = new HashMap<>();
+		cache = new HashMap<>();
 	}
 
 	public int get(int key) {
-		if (!map.containsKey(key)) {
+		Node node = cache.get(key);
+
+		// if key is not in cache return -1
+		if (node == null) {
 			return -1;
 		}
-		Node node = map.get(key);
+
+		// remove node & insert it to head of list
 		remove(node);
 		setHead(node);
 		return node.value;
 	}
 
-	public void set(int key, int value) {
-		if (map.containsKey(key)) {
-			Node node = map.get(key);
+	public void put(int key, int value) {
+		Node node = cache.get(key);
+		if (node != null) {
+
+			// if cache has key, set new value, remove it & insert it to head of list
 			node.value = value;
 			remove(node);
 			setHead(node);
 		} else {
-			if (map.size() == capacity) {
-				map.remove(tail.key);
+
+			// if cache has not key
+			if (cache.size() == capacity) {
+
+				// and cache is full, remove key from cache & remove node at tail (least
+				// recently used)
+				cache.remove(tail.key);
 				remove(tail);
 			}
-			Node node = new Node(key, value);
+
+			// and if cache is not full, create new new node with given key & value, insert
+			// to head & put that key and node in cache
+			node = new Node(key, value);
 			setHead(node);
-			map.put(key, node);
+			cache.put(key, node);
 		}
 	}
 
 	private void remove(Node node) {
+
 		if (node.prev != null) {
+
+			// if node is not head, set it's prev's next to it's next
 			node.prev.next = node.next;
 		} else {
+
+			// else make node's next head
 			head = node.next;
 		}
+
 		if (node.next != null) {
+
+			// if node is not tail, set it's next's prev to it's next
 			node.next.prev = node.prev;
 		} else {
+
+			// else make node's prev tail
 			tail = node.prev;
 		}
 	}
 
 	private void setHead(Node node) {
 		if (head != null) {
+
+			// if head is not null, set it's prev to node
 			head.prev = node;
 		}
+
+		// set node's next & prev
 		node.next = head;
 		node.prev = null;
+
+		// make node head
 		head = node;
 		if (tail == null) {
+
+			// if tail is null make head tail also
 			tail = head;
 		}
 	}
--- src/main/java/me/deepak/interview/lru/LRUCache2.java
+++ src/main/java/me/deepak/interview/lru/LRUCache2.java
@@ -6,22 +6,28 @@ import java.util.Map;
 /*
  * LRU Cache implementation using Linked Hash Map
  * 
+ * https://www.geeksforgeeks.org/lru-cache-implementation/
+ * https://www.geeksforgeeks.org/design-a-data-structure-for-lru-cache/
+ * https://leetcode.com/problems/lru-cache/
  */
-
 public class LRUCache2 {
 
+	// actual cache which holds key & value entries
 	private final LRU<Integer, Integer> lru;
 
+	// creates cache of given capacity
 	public LRUCache2(int capacity) {
 		lru = LRU.newInstance(capacity);
 	}
 
+	// returns value from cache if found, else return -1
 	public int get(int key) {
 		Integer value = lru.get(key);
 		return value == null ? -1 : value;
 	}
 
-	public void set(int key, int value) {
+	// adds entry to cache of given key & value
+	public void put(int key, int value) {
 		lru.put(key, value);
 	}
 
@@ -33,15 +39,31 @@ class LRU<K, V> extends LinkedHashMap<K, V> {
 
 	private final int capacity;
 
+	// static factory method to create new LRU Cache
 	static <K, V> LRU<K, V> newInstance(int capacity) {
 		return new LRU<>(capacity);
 	}
 
+	/*
+	 * Constructs an empty LinkedHashMap instance with the specified initial
+	 * capacity, load factor and ordering mode. accessOrder the ordering mode - true
+	 * for access-order, false for insertion-order
+	 */
 	private LRU(int capacity) {
 		super(capacity, 0.75f, true);
 		this.capacity = capacity;
 	}
 
+	/*
+	 * Returns true if this map should remove its eldest entry. This method is
+	 * invoked by put and putAll after inserting a new entry into the map. It
+	 * provides the implementor with the opportunity to remove the eldest entry each
+	 * time a new one is added. This is useful if the map represents a cache: it
+	 * allows the map to reduce memory consumption by deleting stale entries.
+	 * 
+	 * Returns: true if the eldest entry should be removed from the map; false if it
+	 * should be retained.
+	 */
 	@Override
 	protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
 		return size() > capacity;
