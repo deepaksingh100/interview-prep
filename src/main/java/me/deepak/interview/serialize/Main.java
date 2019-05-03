aceda1a1f55202e73452600fbbc20b1f5d1c9406
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-05-03 16:21:28 +0530

    Serializable done

--- src/main/java/me/deepak/interview/serialize/Main.java
+++ src/main/java/me/deepak/interview/serialize/Main.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+/*
+ * https://docs.oracle.com/javase/10/docs/api/java/io/Serializable.html
+*/
 public class Main {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -14,9 +17,10 @@ public class Main {
 		student.setName("abc");
 		student.setRollno(1);
 		student.setThread(new Thread(() -> System.out.println("In Run")));
+		System.out.println("Serialized : " + student);
 		byte[] bytes = serialize(student);
 		Student deserializedStudent = (Student) deserialize(bytes);
-		System.out.println(deserializedStudent);
+		System.out.println("Deserialized : " + deserializedStudent);
 	}
 
 	private static byte[] serialize(Object object) throws IOException {
--- src/main/java/me/deepak/interview/serialize/Student.java
+++ src/main/java/me/deepak/interview/serialize/Student.java
@@ -7,9 +7,6 @@ import java.io.Serializable;
 
 public class Student implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -158447326349547814L;
 
 	private String name;
@@ -40,11 +37,34 @@ public class Student implements Serializable {
 		this.thread = thread;
 	}
 
+	/*
+	 * The writeObject method is responsible for writing the state of the object for
+	 * its particular class so that the corresponding readObject method can restore
+	 * it. The default mechanism for saving the Object's fields can be invoked by
+	 * calling out.defaultWriteObject. The method does not need to concern itself
+	 * with the state belonging to its super classes or subclasses. State is saved
+	 * by writing the individual fields to the ObjectOutputStream using the
+	 * writeObject method or by using the methods for primitive data types supported
+	 * by DataOutput.
+	 */
 	private void writeObject(ObjectOutputStream objectOutputStream) throws IOException {
 		System.out.println("writeObject in pojo");
 		objectOutputStream.defaultWriteObject();
 	}
 
+	/*
+	 * The readObject method is responsible for reading from the stream and
+	 * restoring the classes fields. It may call in.defaultReadObject to invoke the
+	 * default mechanism for restoring the object's non-static and non-transient
+	 * fields. The defaultReadObject method uses information in the stream to assign
+	 * the fields of the object saved in the stream with the correspondingly named
+	 * fields in the current object. This handles the case when the class has
+	 * evolved to add new fields. The method does not need to concern itself with
+	 * the state belonging to its super classes or subclasses. State is saved by
+	 * writing the individual fields to the ObjectOutputStream using the writeObject
+	 * method or by using the methods for primitive data types supported by
+	 * DataOutput.
+	 */
 	private void readObject(ObjectInputStream objectInputStream) throws IOException, ClassNotFoundException {
 		System.out.println("readObject in pojo");
 		objectInputStream.defaultReadObject();
