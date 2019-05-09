4255500cdd6ff8bdd4ad21ec3f14db9229cef0d7
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-05-09 08:48:22 +0530

    Level, Spiral Order done

--- src/main/java/me/deepak/interview/tree/binary/traversals/LevelOrder.java
+++ src/main/java/me/deepak/interview/tree/binary/traversals/LevelOrder.java
@@ -3,78 +3,124 @@ package me.deepak.interview.tree.binary.traversals;
 import static me.deepak.interview.tree.binary.Height.getHeight;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 import me.deepak.interview.tree.binary.beans.Node;
 
+/*
+ * https://www.geeksforgeeks.org/level-order-tree-traversal/
+ * https://www.geeksforgeeks.org/reverse-level-order-traversal/
+*/
 public class LevelOrder {
 
 	private LevelOrder() {
 	}
 
+	// O(n^2)
 	public static void levelOrder(Node root) {
 		int height = getHeight(root);
+
+		// call level order from top to bottom
 		for (int i = 1; i <= height; i++) {
 			levelOrder(root, i);
 		}
 	}
 
+	// O(n^2)
 	public static void reverseLevelOrder(Node root) {
 		int height = getHeight(root);
+
+		// call level order from bottom to top
 		for (int i = height; i >= 1; i--) {
 			levelOrder(root, i);
 		}
 	}
 
 	private static void levelOrder(Node root, int level) {
+
+		// base case
 		if (root == null) {
 			return;
 		}
+
+		// if we are at given level, print values
 		if (level == 1) {
 			System.out.println(root.getKey() + " ");
 		} else {
+
+			// go to left & right with level - 1
 			levelOrder(root.getLeft(), level - 1);
 			levelOrder(root.getRight(), level - 1);
 		}
 	}
 
 	public static void levelOrderIterative(Node root) {
+
+		// base case
 		if (root == null) {
 			return;
 		}
+
 		Queue<Node> queue = new ArrayDeque<>();
 		queue.add(root);
 		while (!queue.isEmpty()) {
-			root = queue.remove();
-			System.out.print(root.getKey() + " ");
-			if (root.getLeft() != null) {
-				queue.add(root.getLeft());
+			int size = queue.size();
+
+			// print current level nodes in one line
+			while (size-- > 0) {
+				root = queue.remove();
+				System.out.print(root.getKey() + " ");
+
+				// enqueue left child
+				if (root.getLeft() != null) {
+					queue.add(root.getLeft());
+				}
+
+				// enqueue right child
+				if (root.getRight() != null) {
+					queue.add(root.getRight());
+				}
 			}
-			if (root.getRight() != null) {
-				queue.add(root.getRight());
-			}
+			System.out.println();
 		}
 	}
 
 	public static void reverseLevelOrderIterative(Node root) {
+
+		// hold final traversal level by level
+		LinkedList<List<Integer>> traversal = new LinkedList<>();
 		if (root == null) {
 			return;
 		}
 		Queue<Node> queue = new ArrayDeque<>();
-		ArrayDeque<Node> stack = new ArrayDeque<>();
 		queue.add(root);
 		while (!queue.isEmpty()) {
-			root = queue.remove();
-			stack.push(root);
-			if (root.getRight() != null) {
-				queue.add(root.getRight());
+			int size = queue.size();
+			List<Integer> levelTraversal = new ArrayList<>();
+			while (size-- > 0) {
+				root = queue.remove();
+				levelTraversal.add(root.getKey());
+				if (root.getLeft() != null) {
+					queue.add(root.getLeft());
+				}
+				if (root.getRight() != null) {
+					queue.add(root.getRight());
+				}
 			}
-			if (root.getLeft() != null) {
-				queue.add(root.getLeft());
-			}
+
+			// this works as stack, latest level at front
+			traversal.addFirst(levelTraversal);
 		}
-		while (!stack.isEmpty()) {
-			System.out.print(stack.pop().getKey() + " ");
+
+		// print one level per line
+		for (List<Integer> list : traversal) {
+			for (int i : list) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
 		}
 	}
 
--- src/main/java/me/deepak/interview/tree/binary/traversals/SpiralOrder.java
+++ src/main/java/me/deepak/interview/tree/binary/traversals/SpiralOrder.java
@@ -6,14 +6,22 @@ import java.util.ArrayDeque;
 
 import me.deepak.interview.tree.binary.beans.Node;
 
+/*
+ * https://www.geeksforgeeks.org/level-order-traversal-in-spiral-form/
+*/
 public class SpiralOrder {
 
 	private SpiralOrder() {
 	}
 
+	// O(n^2)
 	public static void spiralOrder(Node root) {
 		int height = getHeight(root);
+
+		// true, if we need to traverse from left to right
 		boolean isLeftToRightTraversal = true;
+
+		// traversing top to bottom
 		for (int i = 1; i <= height; i++) {
 			spiralOrder(root, i, isLeftToRightTraversal);
 			isLeftToRightTraversal = !isLeftToRightTraversal;
@@ -21,9 +29,13 @@ public class SpiralOrder {
 	}
 
 	private static void spiralOrder(Node root, int level, boolean isLeftToRightTraversal) {
+
+		// base case
 		if (root == null) {
 			return;
 		}
+
+		// if we are at given level, print values
 		if (level == 1) {
 			System.out.println(root.getKey() + " ");
 		} else {
@@ -37,16 +49,53 @@ public class SpiralOrder {
 		}
 	}
 
-	public static void spiralOrderIterative(Node root) { // first level left to right and so on
+	public static void spiralOrderIterative(Node root) {
+
+		/*
+		 * This algo traverses first (even) level left to right, second (odd) level
+		 * right to left & so on. We are using deque to implement this. If level is
+		 * even, we are doing simple level order traversal. If level is odd, we are
+		 * doing just opposite of even level. As you know for even levels :
+		 * 
+		 * 1. dequeue node from queue (means dequeue from front of queue)
+		 * 
+		 * 2. print data of dequeued node
+		 * 
+		 * 3. if left is not null, enqueue it to queue (means enqueue at rear of queue)
+		 * 
+		 * 4. if right is not null, enqueue it to queue (means enqueue at rear of queue)
+		 * 
+		 * 5. repeat until deque is empty
+		 * 
+		 * Just oppositely, for odd levels :
+		 * 
+		 * 1. dequeue node from rear of queue
+		 * 
+		 * 2. print data of dequeued node
+		 * 
+		 * 3. if right is not null, enqueue it at front of queue
+		 * 
+		 * 4. if left is not null, enqueue it at front of queue
+		 * 
+		 * 5. repeat until deque is empty
+		 */
+
+		// base case
 		if (root == null) {
 			return;
 		}
+
 		ArrayDeque<Node> deque = new ArrayDeque<>();
 		deque.add(root);
-		int level = 0;
+
+		// at even level traverse left to right, else right to left
+		boolean evenLevel = true;
+
 		while (!deque.isEmpty()) {
 			int size = deque.size();
-			if ((level & 1) == 0) {
+			if (evenLevel) {
+
+				// print current even level
 				while (size-- > 0) {
 					root = deque.removeFirst();
 					System.out.print(root.getKey() + " ");
@@ -58,6 +107,8 @@ public class SpiralOrder {
 					}
 				}
 			} else {
+
+				// print current odd level
 				while (size-- > 0) {
 					root = deque.removeLast();
 					System.out.print(root.getKey() + " ");
@@ -69,7 +120,7 @@ public class SpiralOrder {
 					}
 				}
 			}
-			level++;
+			evenLevel = !evenLevel;
 		}
 	}
 
