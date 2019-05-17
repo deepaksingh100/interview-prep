afd60861e6d7017e9e8b2e408edb4cd2f8dd6380
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-05-17 09:50:19 +0530

    Island Perimeter done

--- src/main/java/me/deepak/interview/leetcode/IslandPerimeter.java
+++ src/main/java/me/deepak/interview/leetcode/IslandPerimeter.java
@@ -2,8 +2,8 @@ package me.deepak.interview.leetcode;
 
 /*
  * https://leetcode.com/problems/island-perimeter/
+ * https://www.geeksforgeeks.org/find-perimeter-shapes-formed-1s-binary-matrix/
 */
-
 public class IslandPerimeter {
 
 	public int islandPerimeter(int[][] grid) {
@@ -13,6 +13,8 @@ public class IslandPerimeter {
 		int row = grid.length;
 		int col = grid[0].length;
 		int perimeter = 0;
+
+		// traversing the matrix and finding ones to calculate their contribution.
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
 				if (grid[i][j] == 1) {
@@ -27,15 +29,23 @@ public class IslandPerimeter {
 		int row = grid.length;
 		int col = grid[0].length;
 		int count = 0;
+
+		// up
 		if (i > 0 && grid[i - 1][j] == 1) {
 			count++;
 		}
+
+		// left
 		if (j > 0 && grid[i][j - 1] == 1) {
 			count++;
 		}
+
+		// down
 		if (i < row - 1 && grid[i + 1][j] == 1) {
 			count++;
 		}
+
+		// right
 		if (j < col - 1 && grid[i][j + 1] == 1) {
 			count++;
 		}
