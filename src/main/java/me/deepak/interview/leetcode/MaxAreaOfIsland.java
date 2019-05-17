08beed75d7dfb4fc111543540fac8ed2a2242a54
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-05-17 13:03:40 +0530

    Max Area of Island done

--- src/main/java/me/deepak/interview/leetcode/MaxAreaOfIsland.java
+++ src/main/java/me/deepak/interview/leetcode/MaxAreaOfIsland.java
@@ -3,16 +3,21 @@ package me.deepak.interview.leetcode;
 /*
  * https://leetcode.com/problems/max-area-of-island/
 */
-
 public class MaxAreaOfIsland {
 
 	public int maxAreaOfIsland(int[][] grid) {
 		if (grid == null || grid.length == 0 || grid[0].length == 0) {
 			return 0;
 		}
+
+		// row length
 		int m = grid.length;
+
+		// column length
 		int n = grid[0].length;
+
 		int ans = 0;
+
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n; j++) {
 				if (grid[i][j] == 1) {
@@ -29,7 +34,10 @@ public class MaxAreaOfIsland {
 		if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1) {
 			return 0;
 		}
+
+		// writing an invalid value, so that we would never process this in future
 		grid[i][j] = 2;
+
 		return 1 + merge(grid, i - 1, j) + merge(grid, i + 1, j) + merge(grid, i, j - 1) + merge(grid, i, j + 1);
 	}
 
