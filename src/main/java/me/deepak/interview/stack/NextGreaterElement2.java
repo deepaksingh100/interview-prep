cd1041cae57b4d05f2a75f36418537f1e19ad6c0
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-05-21 12:22:18 +0530

    Next Greater Element done

---
+++ src/main/java/me/deepak/interview/stack/NextGreaterElement2.java
@@ -0,0 +1,30 @@
+package me.deepak.interview.stack;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+/*
+ * https://leetcode.com/problems/next-greater-element-ii/
+*/
+public class NextGreaterElement2 {
+
+	public int[] nextGreaterElements(int[] nums) {
+		int length = nums.length;
+		int[] result = new int[length];
+		Arrays.fill(result, -1);
+		Deque<Integer> stack = new ArrayDeque<>();
+		for (int i = 0; i < 2 * length - 1; i++) {
+			int j = i % length;
+
+			// we need to keep decreasing stack
+			while (!stack.isEmpty() && nums[stack.peek()] < nums[j]) {
+				result[stack.pop()] = nums[j];
+			}
+			stack.push(j);
+		}
+
+		return result;
+	}
+
+}
