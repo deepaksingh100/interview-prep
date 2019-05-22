9a4fae93aed84bfb6afda08b61b8d6c442372354
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-05-22 20:00:49 +0530

    Sudoku done

--- src/main/java/me/deepak/interview/leetcode/SudokuSolver.java
+++ src/main/java/me/deepak/interview/leetcode/SudokuSolver.java
@@ -2,6 +2,7 @@ package me.deepak.interview.leetcode;
 
 /*
  * https://leetcode.com/problems/sudoku-solver/
+ * https://leetcode.com/problems/sudoku-solver/discuss/15752/Straight-Forward-Java-Solution-Using-Backtracking
 */
 public class SudokuSolver {
 
@@ -12,36 +13,47 @@ public class SudokuSolver {
 	private boolean helper(char[][] board) {
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				if (board[i][j] != '.') {
-					continue;
-				}
+				if (board[i][j] == '.') {
 
-				for (char c = '1'; c <= '9'; c++) {
-					if (isValid(board, i, j, c)) {
-						board[i][j] = c;
-						if (helper(board)) {
-							return true;
+					// trial. Try 1 through 9
+					for (char c = '1'; c <= '9'; c++) {
+						if (isValid(board, i, j, c)) {
+
+							// Put c for this cell
+							board[i][j] = c;
+							if (helper(board)) {
+
+								// If it's the solution return true
+								return true;
+							}
+
+							// Otherwise go back
+							board[i][j] = '.';
 						}
-						board[i][j] = '.';
 					}
+					return false;
 				}
-				return false;
 			}
 		}
 
-		return true; // return true if all cells are checked
+		// return true if all cells are checked
+		return true;
 	}
 
 	private boolean isValid(char[][] board, int row, int col, char c) {
 		for (int i = 0; i < 9; i++) {
+
+			// check in column
 			if (board[i][col] == c) {
 				return false;
 			}
 
+			// check in row
 			if (board[row][i] == c) {
 				return false;
 			}
 
+			// check in 3x3 block
 			if (board[row / 3 * 3 + i / 3][col / 3 * 3 + i % 3] == c) {
 				return false;
 			}
--- src/main/java/me/deepak/interview/leetcode/ValidSudoku.java
+++ src/main/java/me/deepak/interview/leetcode/ValidSudoku.java
@@ -13,40 +13,40 @@ public class ValidSudoku {
 
 		// check each row
 		for (int i = 0; i < 9; i++) {
-			boolean[] m = new boolean[9];
+			boolean[] exist = new boolean[9];
 			for (int j = 0; j < 9; j++) {
 				if (board[i][j] != '.') {
-					if (m[(int) (board[i][j] - '1')]) {
+					if (exist[board[i][j] - '1']) {
 						return false;
 					}
-					m[(int) (board[i][j] - '1')] = true;
+					exist[board[i][j] - '1'] = true;
 				}
 			}
 		}
 
 		// check each column
 		for (int j = 0; j < 9; j++) {
-			boolean[] m = new boolean[9];
+			boolean[] exist = new boolean[9];
 			for (int i = 0; i < 9; i++) {
 				if (board[i][j] != '.') {
-					if (m[(int) (board[i][j] - '1')]) {
+					if (exist[board[i][j] - '1']) {
 						return false;
 					}
-					m[(int) (board[i][j] - '1')] = true;
+					exist[board[i][j] - '1'] = true;
 				}
 			}
 		}
 
-		// check each 3x3 matrix
+		// check each 3x3 block
 		for (int block = 0; block < 9; block++) {
-			boolean[] m = new boolean[9];
+			boolean[] exist = new boolean[9];
 			for (int i = block / 3 * 3; i < block / 3 * 3 + 3; i++) {
 				for (int j = block % 3 * 3; j < block % 3 * 3 + 3; j++) {
 					if (board[i][j] != '.') {
-						if (m[(int) (board[i][j] - '1')]) {
+						if (exist[board[i][j] - '1']) {
 							return false;
 						}
-						m[(int) (board[i][j] - '1')] = true;
+						exist[board[i][j] - '1'] = true;
 					}
 				}
 			}
