2ddbab03b745fe828f27f55656d573087132b63d
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-05-22 08:11:24 +0530

    Valid parentheses done

---
+++ src/main/java/me/deepak/interview/stack/ValidParentheses.java
@@ -0,0 +1,43 @@
+package me.deepak.interview.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/*
+ * https://leetcode.com/problems/valid-parentheses/
+ * https://leetcode.com/articles/valid-parentheses/
+*/
+public class ValidParentheses {
+	public boolean isValid(String s) {
+		Deque<Character> stack = new ArrayDeque<>();
+		for (char c : s.toCharArray()) {
+			if (isOpeningBracket(c)) {
+				stack.push(c);
+			} else {
+				if (stack.isEmpty() || c != getClosingBracket(stack.pop())) {
+					return false;
+				}
+			}
+		}
+		return stack.isEmpty();
+	}
+
+	private boolean isOpeningBracket(char c) {
+		return c == '(' || c == '{' || c == '[';
+	}
+
+	private char getClosingBracket(char c) {
+		switch (c) {
+		case '(':
+			return ')';
+		case '{':
+			return '}';
+		case '[':
+			return ']';
+		default:
+			throw new IllegalArgumentException("Invalid char " + c);
+
+		}
+	}
+
+}
