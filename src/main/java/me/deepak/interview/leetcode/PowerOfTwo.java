466c28267e21ae45f6b309ca83335317c6b1eda1
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-05-23 17:24:59 +0530

    power of 2 done

---
+++ src/main/java/me/deepak/interview/leetcode/PowerOfTwo.java
@@ -0,0 +1,12 @@
+package me.deepak.interview.leetcode;
+
+/*
+ * https://leetcode.com/problems/power-of-two/
+*/
+public class PowerOfTwo {
+
+	public boolean isPowerOfTwo(int n) {
+		return n > 0 && (n & (n - 1)) == 0;
+	}
+
+}
