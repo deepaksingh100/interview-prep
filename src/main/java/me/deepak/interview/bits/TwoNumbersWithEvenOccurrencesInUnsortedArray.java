5f114adf8241d96209d31fa22a73f55685af1811
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-05-26 10:07:11 +0530

    Find the two (non) repeating elements in a given array

---
+++ src/main/java/me/deepak/interview/bits/TwoNumbersWithEvenOccurrencesInUnsortedArray.java
@@ -0,0 +1,84 @@
+package me.deepak.interview.bits;
+
+/*
+ * https://www.geeksforgeeks.org/find-the-two-repeating-elements-in-a-given-array/
+ * https://stackoverflow.com/questions/22952651/explain-using-xor-to-find-two-non-duplicate-integers-in-an-array/22953668#22953668
+*/
+public class TwoNumbersWithEvenOccurrencesInUnsortedArray {
+
+	private TwoNumbersWithEvenOccurrencesInUnsortedArray() {
+	}
+
+	/*
+	 * Prints two numbers that occur even number of times.
+	 */
+	public static int[] printTwoOdd(int[] arr) {
+
+		int length = arr.length;
+
+		// from 1 to n numbers are present arr[]
+		int n = length - 2;
+
+		// will hold XOR of two odd occurring elements
+		int xor = 0;
+
+		/*
+		 * get the XOR of all elements in arr[] & all possible elements. The xor will
+		 * basically be XOR of two even occurring elements. By second loop we are
+		 * ensuring that even occurring elements get XORed odd times.
+		 */
+		for (int i = 0; i < length; i++) {
+			xor ^= arr[i];
+		}
+
+		for (int i = 1; i <= length; i++) {
+			xor ^= i;
+		}
+
+		/*
+		 * get one set bit in the xor. We get rightmost set bit in the following line as
+		 * it is easy to get.
+		 */
+		int rightMostSetBitNumber = xor & -xor;
+
+		// two even occurring elements
+		int x = 0;
+		int y = 0;
+
+		/*
+		 * Now divide elements in two sets:
+		 * 
+		 * 1) The elements having the corresponding bit as 1.
+		 * 
+		 * 2) The elements having the corresponding bit as 0.
+		 */
+		for (int i = 0; i < length; i++) {
+
+			if ((arr[i] & rightMostSetBitNumber) == 0) {
+
+				// XOR of first set in arr[]
+				x ^= arr[i];
+			} else {
+
+				// XOR of second set in arr[]
+				y ^= arr[i];
+			}
+		}
+
+		for (int i = 1; i <= n; i++) {
+
+			if ((i & rightMostSetBitNumber) == 0) {
+
+				// XOR of first set in arr[] and {1, 2, ...n }
+				x ^= i;
+			} else {
+
+				// XOR of second set in arr[] and {1, 2, ...n }
+				y ^= i;
+			}
+		}
+
+		return new int[] { x, y };
+	}
+
+}
---
+++ src/main/java/me/deepak/interview/bits/TwoNumbersWithOddOccurrencesInUnsortedArray.java
@@ -0,0 +1,66 @@
+package me.deepak.interview.bits;
+
+/*
+ * https://www.geeksforgeeks.org/find-the-two-numbers-with-odd-occurences-in-an-unsorted-array/
+ * https://www.geeksforgeeks.org/find-two-non-repeating-elements-in-an-array-of-repeating-elements/
+ * https://stackoverflow.com/questions/22952651/explain-using-xor-to-find-two-non-duplicate-integers-in-an-array/22953668#22953668
+*/
+public class TwoNumbersWithOddOccurrencesInUnsortedArray {
+
+	private TwoNumbersWithOddOccurrencesInUnsortedArray() {
+	}
+
+	/*
+	 * Prints two numbers that occur odd number of times. The function assumes that
+	 * the array length is at least 2 and there are exactly two numbers occurring
+	 * odd number of times.
+	 */
+	public static int[] printTwoOdd(int[] arr) {
+
+		int length = arr.length;
+
+		// will hold XOR of two odd occurring elements
+		int xor = 0;
+
+		/*
+		 * get the XOR of all elements in arr[]. The xor will basically be XOR of two
+		 * odd occurring elements.
+		 */
+		for (int i = 0; i < length; i++) {
+			xor ^= arr[i];
+		}
+
+		/*
+		 * get one set bit in the xor. We get rightmost set bit in the following line as
+		 * it is easy to get.
+		 */
+		int rightMostSetBitNumber = xor & -xor;
+
+		// two odd occurring elements
+		int x = 0;
+		int y = 0;
+
+		/*
+		 * Now divide elements in two sets:
+		 * 
+		 * 1) The elements having the corresponding bit as 1.
+		 * 
+		 * 2) The elements having the corresponding bit as 0.
+		 */
+		for (int i = 0; i < length; i++) {
+
+			if ((arr[i] & rightMostSetBitNumber) == 0) {
+
+				// XOR of first set in arr[]
+				x ^= arr[i];
+			} else {
+
+				// XOR of second set in arr[]
+				y ^= arr[i];
+			}
+		}
+
+		return new int[] { x, y };
+	}
+
+}
