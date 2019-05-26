87ab1395686acef38c74f36bf27ca8ed71e9837e
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-05-26 17:37:06 +0530

    Ancestors

---
+++ src/main/java/me/deepak/interview/tree/binary/Ancestors.java
@@ -0,0 +1,44 @@
+package me.deepak.interview.tree.binary;
+
+import java.util.List;
+
+import me.deepak.interview.tree.binary.beans.Node;
+
+/*
+ * https://www.geeksforgeeks.org/print-ancestors-of-a-given-node-in-binary-tree/
+*/
+public class Ancestors {
+
+	private Ancestors() {
+	}
+
+	/*
+	 * If target is present in tree, then prints the ancestors and returns true,
+	 * otherwise returns false.
+	 */
+	public static boolean getAncestors(Node root, int key, List<Integer> ancestors) {
+
+		// base case
+		if (root == null) {
+			return false;
+		}
+
+		if (root.getKey() == key) {
+			return true;
+		}
+
+		/*
+		 * If target is present in either left or right subtree of this node, then print
+		 * this node
+		 */
+		if (getAncestors(root.getLeft(), key, ancestors) || getAncestors(root.getRight(), key, ancestors)) {
+			ancestors.add(root.getKey());
+			return true;
+		}
+
+		/* else return false */
+		return false;
+
+	}
+
+}
