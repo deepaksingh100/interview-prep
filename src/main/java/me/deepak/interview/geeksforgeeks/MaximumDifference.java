2f04fe67cfbd90c6e16d19d9891e44500a082231
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-06-06 15:21:09 +0530

    Max Diff done

--- src/main/java/me/deepak/interview/geeksforgeeks/MaximumDifference.java
+++ src/main/java/me/deepak/interview/geeksforgeeks/MaximumDifference.java
@@ -8,11 +8,11 @@ public class MaximumDifference {
 	private MaximumDifference() {
 	}
 
-	public static int maxDiff(int[] a, int n) {
+	public static int maxDiff(int[] a) {
 		int maxDiff = a[1] - a[0];
 		int minElement = a[0];
 
-		for (int i = 1; i < n; i++) {
+		for (int i = 1; i < a.length; i++) {
 			maxDiff = Math.max(maxDiff, a[i] - minElement);
 			minElement = Math.min(minElement, a[i]);
 		}
