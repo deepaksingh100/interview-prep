5768c54e2b087322ef475118812b2e324c9414a8
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-06-16 19:37:36 +0530

    Fibonacci done

---
+++ src/main/java/me/deepak/interview/dynamic_programming/Fibonacci.java
@@ -0,0 +1,29 @@
+package me.deepak.interview.dynamic_programming;
+
+public class Fibonacci {
+
+	private Fibonacci() {
+	}
+
+	public static int fibonacci(int n) {
+
+		// declare an array to store Fibonacci numbers.
+		int[] fibonaccis = new int[n + 2]; // 1 extra to handle case, n = 0
+		int i;
+
+		// 0th and 1st number of the series are 0 and 1
+		fibonaccis[0] = 0;
+		fibonaccis[1] = 1;
+
+		for (i = 2; i <= n; i++) {
+
+			/*
+			 * Add the previous 2 numbers in the series and store it
+			 */
+			fibonaccis[i] = fibonaccis[i - 1] + fibonaccis[i - 2];
+		}
+
+		return fibonaccis[n];
+	}
+
+}
