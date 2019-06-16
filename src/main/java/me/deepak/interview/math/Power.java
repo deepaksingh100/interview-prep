71abd0799241ab3f5b95eec53db6f006889333a9
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-06-16 14:08:32 +0530

    Power method

---
+++ src/main/java/me/deepak/interview/math/Power.java
@@ -0,0 +1,54 @@
+package me.deepak.interview.math;
+
+/*
+ * https://www.geeksforgeeks.org/write-a-c-program-to-calculate-powxn/
+ * https://www.geeksforgeeks.org/write-an-iterative-olog-y-function-for-powx-y/
+*/
+public class Power {
+
+	private Power() {
+	}
+
+	public static double power(double x, int y) {
+		if (y == 0) {
+			return 1;
+		}
+
+		double temp = power(x, y / 2);
+
+		if ((y & 1) == 0) {
+			return temp * temp;
+		}
+
+		if (y > 0) {
+			return x * temp * temp;
+		}
+		return (temp * temp) / x;
+	}
+
+	public static double powerIterative(double x, int y) {
+
+		int result = 1;
+
+		if (y > 0) {
+			while (y > 0) {
+				if ((y & 1) == 1) {
+					result *= x;
+				}
+				y >>= 1;
+				x *= x;
+			}
+		} else if (y < 0) {
+			while (y < 0) {
+				if ((y & 1) == 1) {
+					result /= x;
+				}
+				y >>= 1;
+				x *= x;
+			}
+		}
+
+		return result;
+	}
+
+}
