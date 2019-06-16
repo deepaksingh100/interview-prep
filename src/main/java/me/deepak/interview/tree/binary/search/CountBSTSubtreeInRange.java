7582154016131ddd3aa59e9597a724c028b4f056
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-06-16 18:46:50 +0530

    CountBSTSubtreeInRange

---
+++ src/main/java/me/deepak/interview/tree/binary/search/CountBSTSubtreeInRange.java
@@ -0,0 +1,44 @@
+package me.deepak.interview.tree.binary.search;
+
+import me.deepak.interview.tree.binary.beans.Node;
+
+/*
+ * https://www.geeksforgeeks.org/count-bst-subtrees-that-lie-in-given-range/
+*/
+public class CountBSTSubtreeInRange {
+
+	private CountBSTSubtreeInRange() {
+	}
+
+	private static int count = 0;
+
+	public static Integer getCount(Node root, int low, int high) {
+		count(root, low, high);
+		return count;
+	}
+
+	// A recursive function to get count of nodes whose subtree is in range from low
+	// to high. This function returns true if nodes in subtree rooted under 'root'
+	// are in range.
+	private static boolean count(Node root, int low, int high) {
+
+		// base case
+		if (root == null) {
+			return true;
+		}
+
+		// recur for left and right subtrees
+		boolean l = count(root.getLeft(), low, high);
+		boolean r = count(root.getRight(), low, high);
+
+		// if both left and right subtrees are in range and current Node is also in
+		// range, then increment count and return true
+		if (l && r && low <= root.getKey() && root.getKey() <= high) {
+			count++;
+			return true;
+		}
+
+		return false;
+	}
+
+}
