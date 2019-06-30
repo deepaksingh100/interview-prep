98301ea02e0c4aebbb7cf404bbcd4db7881cb1a7
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-06-30 15:26:33 +0530

    right view of binary tree recursive

--- src/main/java/me/deepak/interview/leetcode/BinaryTreeRightSideView.java
+++ src/main/java/me/deepak/interview/leetcode/BinaryTreeRightSideView.java
@@ -8,11 +8,35 @@ import java.util.Queue;
 import me.deepak.interview.leetcode.beans.TreeNode;
 
 /*
+ * https://www.geeksforgeeks.org/print-right-view-binary-tree-2/
  * https://leetcode.com/problems/binary-tree-right-side-view/
 */
 public class BinaryTreeRightSideView {
 
+	private int maxLevel = 0;
+
 	public List<Integer> rightSideView(TreeNode root) {
+		List<Integer> view = new ArrayList<>();
+		rightSideView(root, 1, view);
+		return view;
+	}
+
+	private void rightSideView(TreeNode root, int level, List<Integer> view) {
+		if (root == null) {
+			return;
+		}
+
+		if (maxLevel < level) {
+			view.add(root.val);
+			maxLevel = level;
+		}
+
+		rightSideView(root.right, level + 1, view);
+		rightSideView(root.left, level + 1, view);
+
+	}
+
+	public List<Integer> rightSideViewIterative(TreeNode root) {
 		List<Integer> rightView = new ArrayList<>();
 
 		if (root == null) {
