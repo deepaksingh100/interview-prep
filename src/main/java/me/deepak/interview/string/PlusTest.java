1c43a137432cd3800838ca61514977beed5a38f5
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-07-06 14:45:01 +0530

    Plus Operator test string

---
+++ src/main/java/me/deepak/interview/string/PlusTest.java
@@ -0,0 +1,17 @@
+package me.deepak.interview.string;
+
+/*
+ * https://youtu.be/6pLEwJP1Afk?t=1964
+ * We can use + operator for concatenation as string in java, as + operator is overloaded as 
+ * StringBuilder.append. Run command javap -c PlusTest.class & examine the output.
+ * 
+*/
+public class PlusTest {
+
+	public static void main(String[] args) {
+
+		String s = "This " + "is " + Boolean.valueOf(true) + " plus " + "Test.";
+		System.out.println(s);
+	}
+
+}
