2fc061b2e33f03e6c0bd9496612e86879575829f
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-07-08 18:11:00 +0530

    reason add : why bill pugh singleton is thread safe

--- src/main/java/me/deepak/interview/design_patterns/creational/singleton/BillPughSingleton.java
+++ src/main/java/me/deepak/interview/design_patterns/creational/singleton/BillPughSingleton.java
@@ -1,10 +1,18 @@
 package me.deepak.interview.design_patterns.creational.singleton;
 
+/*
+ * https://stackoverflow.com/questions/6109896/singleton-pattern-bill-pughs-solution
+*/
 public class BillPughSingleton {
 
 	private BillPughSingleton() {
 	}
 
+	/*
+	 * The JLS guarantees that a class is only loaded when it's used for the first
+	 * time (making the singleton initialization lazy), and that the class loading
+	 * is thread-safe (making the getInstance() method thread-safe as well)
+	 */
 	private static final class SingletonHelper {
 		private static final BillPughSingleton INSTANCE = new BillPughSingleton();
 	}
