02e68115e74f0d4841b3145b0bf55214c1aa177e
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-07-18 13:08:52 +0530

    comment improved for fibonacci

--- src/main/java/me/deepak/interview/dynamic_programming/Fibonacci.java
+++ src/main/java/me/deepak/interview/dynamic_programming/Fibonacci.java
@@ -7,8 +7,9 @@ public class Fibonacci {
 
 	public static int fibonacci(int n) {
 
-		// declare an array to store Fibonacci numbers.
-		int[] fibonaccis = new int[n + 2]; // 1 extra to handle case, n = 0
+		// declare an array to store fibonacci numbers.
+		// 1 extra to handle case, n = 0
+		int[] fibonaccis = new int[n + 1];
 
 		// 0th and 1st number of the series are 0 and 1
 		fibonaccis[0] = 0;
@@ -16,9 +17,7 @@ public class Fibonacci {
 
 		for (int i = 2; i <= n; i++) {
 
-			/*
-			 * Add the previous 2 numbers in the series and store it
-			 */
+			// add the previous 2 numbers in the series and store it
 			fibonaccis[i] = fibonaccis[i - 1] + fibonaccis[i - 2];
 		}
 
