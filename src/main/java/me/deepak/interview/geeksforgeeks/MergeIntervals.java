4100f100a4b165fc52731af350447bdef35b6b4f
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-07-21 23:44:56 +0530

    Comments added

--- src/main/java/me/deepak/interview/geeksforgeeks/MergeIntervals.java
+++ src/main/java/me/deepak/interview/geeksforgeeks/MergeIntervals.java
@@ -14,13 +14,25 @@ public class MergeIntervals {
 			return new int[][] {};
 		}
 
+		// sorting array in ascending order of start time
 		Arrays.sort(intervals, (o1, o2) -> Integer.compare(o1[0], o2[0]));
 
+		// index will store last index of merged intervals
 		int index = 0;
+
+		// first interval will always there in answer, so starting from i = 1
 		for (int i = 1; i < intervals.length; i++) {
+
 			if (intervals[index][1] < intervals[i][0]) {
+
+				// if last merged interval's end is smaller than start of current interval
+				// this is a new interval, increase index & put this interval
 				intervals[++index] = intervals[i];
 			} else if (intervals[index][1] < intervals[i][1]) {
+
+				// else if last merged interval's end is smaller than end of current interval
+				// this interval should be merged with the previous one, update end of interval
+				// at index
 				intervals[index][1] = intervals[i][1];
 			}
 		}
