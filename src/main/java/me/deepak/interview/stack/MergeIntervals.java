7fae2b364e91228292f08040e8a7b3be7db9c831
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-07-21 23:19:35 +0530

    Merge Intervals done

--- src/main/java/me/deepak/interview/stack/MergeIntervals.java
+++ src/main/java/me/deepak/interview/stack/MergeIntervals.java
@@ -1,6 +1,8 @@
 package me.deepak.interview.stack;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 
 /*
  * https://leetcode.com/problems/merge-intervals/
@@ -15,17 +17,28 @@ public class MergeIntervals {
 		}
 
 		Arrays.sort(intervals, (o1, o2) -> Integer.compare(o1[0], o2[0]));
-
-		int index = 0;
+		Deque<int[]> stack = new ArrayDeque<>();
+		stack.push(intervals[0]);
 		for (int i = 1; i < intervals.length; i++) {
-			if (intervals[index][1] < intervals[i][0]) {
-				intervals[++index] = intervals[i];
-			} else if (intervals[index][1] < intervals[i][1]) {
-				intervals[index][1] = intervals[i][1];
+			int[] top = stack.peek();
+			if (top[1] < intervals[i][0]) {
+				stack.push(intervals[i]);
+			} else if (top[1] < intervals[i][1]) {
+				top[1] = intervals[i][1];
 			}
 		}
-
-		return Arrays.copyOfRange(intervals, 0, index + 1);
+		StringBuilder result = new StringBuilder();
+		while (!stack.isEmpty()) {
+			int[] interval = stack.pop();
+			result.insert(0, interval[1]).insert(0, " ").insert(0, interval[0]).insert(0, " ");
+		}
+		String[] temp = result.substring(1).split(" ");
+		int[][] a = new int[temp.length / 2][2];
+		for (int i = 0; i < temp.length; i += 2) {
+			a[i / 2][0] = Integer.parseInt(temp[i]);
+			a[i / 2][1] = Integer.parseInt(temp[i + 1]);
+		}
+		return a;
 	}
 
 }
