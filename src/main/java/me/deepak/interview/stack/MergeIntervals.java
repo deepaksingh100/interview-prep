3dac36fbfe0ab9fcd54cc184a7afaf5123be963d
YATRAONLINE\deepak.singh2 <singhdpk100@gmail.com>
2019-07-21 23:18:01 +0530

    Merge intervals done

---
+++ src/main/java/me/deepak/interview/stack/MergeIntervals.java
@@ -0,0 +1,31 @@
+package me.deepak.interview.stack;
+
+import java.util.Arrays;
+
+/*
+ * https://leetcode.com/problems/merge-intervals/
+ * https://www.geeksforgeeks.org/merging-intervals/
+*/
+public class MergeIntervals {
+
+	public int[][] merge(int[][] intervals) {
+
+		if (intervals == null || intervals.length == 0 || intervals[0].length == 0) {
+			return new int[][] {};
+		}
+
+		Arrays.sort(intervals, (o1, o2) -> Integer.compare(o1[0], o2[0]));
+
+		int index = 0;
+		for (int i = 1; i < intervals.length; i++) {
+			if (intervals[index][1] < intervals[i][0]) {
+				intervals[++index] = intervals[i];
+			} else if (intervals[index][1] < intervals[i][1]) {
+				intervals[index][1] = intervals[i][1];
+			}
+		}
+
+		return Arrays.copyOfRange(intervals, 0, index + 1);
+	}
+
+}
